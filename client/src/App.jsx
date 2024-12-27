f14e9f2d2b5075a0ef82afe303e83132cf0092c6
Peter Thornton <peter@MacBook-Pro.local>
2024-12-27 15:15:40 -0500

    boilerplate front end tailwindcss and react, build processes working

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 p-10">
+      <h1 className="text-4xl font-bold text-white">Tailwind Test</h1>
+    </div>
+  );
+};
+
+export default App;
