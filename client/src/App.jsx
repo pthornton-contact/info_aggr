994b324234d9f149127afa85184f8d4adc173903
Peter Thornton <peter@MacBook-Pro.local>
2024-12-27 18:56:29 -0500

    saving changes to work on bug with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import FeedList from "./components/FeedList";
 
-const App = () => {
+function App() {
   return (
-    <div className="bg-blue-500 p-10">
-      <h1 className="text-4xl font-bold text-white">Tailwind Test</h1>
+    <div className="bg-gray-50 min-h-screen">
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-center text-3xl font-bold">Feeds Dashboard</h1>
+      </header>
+      <main className="container mx-auto py-6">
+        <FeedList />
+      </main>
     </div>
   );
-};
+}
 
 export default App;
